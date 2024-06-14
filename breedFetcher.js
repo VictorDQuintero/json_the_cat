4288f010199de35572a895aae307016c292e967d
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-14 13:57:44 -0600

    Modify code to be able to handle a callback function, Create descriptionPrinter function, which acts as callback. Modify code to handle failed requests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,33 +1,35 @@
 const needle = require("needle");
 
-const breedFetcher = function (){
+// Function that takes a callback function and gives it the response.body of a user specified cat breed from a needle.get search request
+// from thecatapi.com
+const breedFetcher = function(callback) {
  
-const breed = process.argv[2];
+  const breed = process.argv[2]; // takes user provided cat breed
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`; // url to search that specific cat breed
 
 
-needle.get(url, (error, response) => {
-  if(error){
-    console.log("error:", error); // Print the error if one occurred
-  }
+  needle.get(url, (error, response) => { // sends get request to the catapi.com
+    if (error) {
+      console.log("error:", error); // Print the error if one occurred
+    } else {
+      callback(response.body); // sends response.body and breed to callback function
+    }
+
+  });
+};
+
+//Function that prints out the description of a user provided cat breed
+const descriptionPrinter = function(data) {
   
-   
-  const data = response.body;
-  
-  if (data.length === 0){
+  if (data.length === 0) { // If array is empty then the cat breed wasn't found
     console.log(`Cat breed not found.`);
   } else {
-  console.log(`${breed} breed description: ` + data[0].description);
+    console.log(`${data[0].name} breed description: "${data[0].description}"`); // prints out breed name and description
   }
 
-});
+};
 
 
 
-}
-
-
-
-
-breedFetcher();
+breedFetcher(descriptionPrinter);
