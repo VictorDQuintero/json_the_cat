c0cfd7c6dc6760a9487d3a7715355cf64fadd075
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-14 13:29:53 -0600

    Add code to have user enter desired cat breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,28 @@
 const needle = require("needle");
 
-needle.get("https://api.thecatapi.com/v1/breeds/search?q=siberian", (error, response, body) => {
-  // console.log("error:", error); // Print the error if one occurred
-  // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  console.log("first entry of body:", body[0].description); // Print the HTML for the Google homepage.
-  console.log("------");
-  console.log(typeof body);
+const breedFetcher = function (){
+ 
+const breed = process.argv[2];
+
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+
+needle.get(url, (error, response) => {
+  if(error){
+    console.log("error:", error); // Print the error if one occurred
+  }
+  
+   
+  const data = response.body;
+  console.log(`${breed} breed description: ` + data[0].description);
+
 });
+
+
+
+}
+
+
+
+
+breedFetcher();
